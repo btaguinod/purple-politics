a4012fcfb07c375f8d1641296f33ae5ef3674358
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-06-19 08:54:48 -0700

    Add Article docstring

--- backend/article/article.py
+++ backend/article/article.py
@@ -1,6 +1,16 @@
 
 
 class Article:
+    """Representation of online news article.
+
+    Attributes:
+        company (str): Company name.
+        title (str): Article title.
+        description (str): First lines of article.
+        article_url (str): Link to article.
+        image_url (str): Link to article thumbnail.
+        published_time (str): Time article was posted in ISO 8601 format
+    """
     def __init__(self, company: str, title: str, description: str,
                  article_url: str, image_url: str, published_time: str):
         self.company = company
