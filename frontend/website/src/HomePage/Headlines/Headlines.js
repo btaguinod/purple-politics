5a6566fdcf4257bbb34275b9123bdce82a40ed81
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-06-30 20:47:13 -0700

    Add top headlines

--- frontend/website/src/HomePage/Headlines/Headlines.js
+++ frontend/website/src/HomePage/Headlines/Headlines.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import './Headlines.css'
 
 export default class Headlines extends Component {
     render() {
         return (
-            <div>
-                
+            <div id="headlines">
+                <div id="headlines-label">Top Headlines</div>
+                {this.props.articles.map(article => 
+                    <div className="headline-title">{article.title}</div>
+                )}
             </div>
         )
     }
