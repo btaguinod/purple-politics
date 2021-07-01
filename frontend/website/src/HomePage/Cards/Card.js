de5f690be21cca14862a2092dd8a60de36b2d4ab
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-06-30 23:59:51 -0700

    Add different article length support

--- frontend/website/src/HomePage/Cards/Card.js
+++ frontend/website/src/HomePage/Cards/Card.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
 
 export default class Card extends Component {
+    static titleSizes = {100:'sm', 90:'md', 60:'lg', 45:'xl'}
+    constructor(props) {
+        super(props);
+        let titleLength = this.props.article.title.length;
+        let closestSize = Object.keys(Card.titleSizes).reduce((prev, curr) => 
+            Math.abs(Number(curr) - titleLength) < Math.abs(Number(prev) - titleLength) ? curr : prev
+        )
+
+        console.log(closestSize)
+
+        this.titleClass = "card-title-" + this.props.type + ' title-' + String(Card.titleSizes[closestSize]) + " " + String(titleLength);
+    }
     render() {
+        
         return (
             <div className={"card-base-" + this.props.type}>
                 <div className={"card-image-" + this.props.type}>
@@ -11,14 +24,16 @@ export default class Card extends Component {
                     />
                 </div>
                 <div className={"card-text-" + this.props.type}>
-                    <div className={"card-title-" + this.props.type}>
+                    <div className={this.titleClass}>
                         {this.props.article.title}
                     </div>
-                    <div className={"card-date-" + this.props.type}>
-                        {this.props.article.date}
-                    </div>
-                    <div className={"card-sources-" + this.props.type}>
-                        {this.props.article.sources.join(" | ")}
+                    <div>
+                        <div className={"card-date-" + this.props.type}>
+                            {this.props.article.date}
+                        </div>
+                        <div className={"card-sources-" + this.props.type}>
+                            {this.props.article.sources.join(" | ")}
+                        </div>
                     </div>
                 </div>
             </div>
