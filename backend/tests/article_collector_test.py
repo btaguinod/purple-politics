b7e5ec87d604a9908c8e1f57399e2859f98d4805
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-07-12 13:12:31 -0700

    Fix article collector test

--- backend/tests/article_collector_test.py
+++ backend/tests/article_collector_test.py
@@ -1,25 +1,12 @@
 import unittest
-from datetime import datetime
 
 from article import Article
 from article_collector import ArticleCollector
 
+from company import companies
+
 
 class MyTestCase(unittest.TestCase):
-    URLS = {
-        'CNN': 'http://rss.cnn.com/rss/cnn_allpolitics.rss',
-        'HuffPost': 'https://chaski.huffpost.com/us/auto/vertical/politics',
-        'LA Times': 'https://www.latimes.com/politics/rss2.0.xml#nt=1col'
-                    '-7030col1',
-        'CBS News': 'https://www.cbsnews.com/latest/rss/politics',
-        'BBC News':  'http://feeds.bbci.co.uk/news/world/us_and_canada/rss'
-                     '.xml',
-        'Epoch Times': 'https://www.theepochtimes.com/c-us-politics/feed',
-        'National Review': 'https://www.nationalreview.com/politics-policy'
-                           '/feed/',
-        'Fox News': 'http://feeds.foxnews.com/foxnews/politics',
-        'Daily Mail': 'https://www.dailymail.co.uk/news/us-politics/index.rss'
-    }
     MAX_ARTICLES = 100
 
     OUTPUT_MAX = 5
@@ -27,8 +14,10 @@ class MyTestCase(unittest.TestCase):
     def test_article_collector(self):
         print('Testing article collector...')
 
-        for company, url in self.URLS.items():
-            print('Testing ' + company + ':')
+        for item in companies:
+            company = item['company']
+            url = item['url']
+            print('Testing ' + str(company) + ':')
             collector = ArticleCollector(company, url)
             articles = collector.get_articles(100)
             self.assertIsInstance(articles, list)
