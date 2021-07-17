47ebdd17e63d0c83d30e389447b7b788e8d7cc73
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-07-16 23:02:36 -0700

    Update endpoints

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
 from flask_cors import CORS
+from datetime import datetime
 
 import os
 try:
@@ -19,18 +20,28 @@ db = mongoClient.get_database('purplePolitics')
 collection = db.get_collection('events')
 
 
-@app.route('/articles/<event_id>/')
+@app.route('/articles/<event_id>')
 def get_articles(event_id):
     event = collection.find_one({"eventId": event_id})
-    event['_id'] = str(event['_id'])
-    return jsonify(event)
+    return jsonify(event['articles'])
 
 
-@app.route('/events/')
+@app.route('/events')
 def get_events():
-    events = [event for event in collection.find({})]
-    for event in events:
-        event['_id'] = str(event['_id'])
+    events = []
+    for event in collection.find():
+        articles = event['articles']
+        companies = set([article['company']['name'] for article in articles])
+        earliest_article = min(articles, key=lambda x: x['publishedTime'])
+        latest_article = max(articles, key=lambda x: x['publishedTime'])
+        events.append({
+            'eventId': event['eventId'],
+            'title': earliest_article['title'],
+            'imageUrl': earliest_article['imageUrl'],
+            'earliestTime': earliest_article['publishedTime'],
+            'latestTime': latest_article['publishedTime'],
+            'companies': list(companies)
+        })
     return jsonify(events)
 
 
