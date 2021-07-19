4a409d51639363423b98fbf994ff56ccdf72ea9e
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-07-19 12:33:26 -0700

    Add mobile popup menu

--- frontend/website/src/Header/Header.js
+++ frontend/website/src/Header/Header.js
@@ -10,6 +10,16 @@ export default class Header extends Component {
                     <Link id="logo" to="/">
                         <span id="purple">PURPLE</span> POLITICS
                     </Link>
+                    <div id="button-mobile">
+                        <input type="checkbox" id="button-checkbox-mobile" />
+                        <div id="page-links-mobile">
+                        <Link className="page-link-mobile" to="/"> Home </Link>
+                        <Link className="page-link-mobile" to="/about"> About </Link>
+                        </div>
+                        <span />
+                        <span />
+                        <span />
+                    </div>
                     <div id="page-links">
                         <Link className="page-link" to="/"> Home </Link>
                         <Link className="page-link" to="/about"> About </Link>
