2b15b78bd6cc8639bef8a4a8f2c1bb3906011f23
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-07-25 21:56:10 -0700

    Add query parameters

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from pymongo import MongoClient
 from flask_cors import CORS
-from flask_restful import Resource, Api, request
+from flask_restful import Resource, Api, request, abort
 
 import os
 try:
@@ -22,13 +22,68 @@ collection = db.get_collection('events')
 
 
 class Articles(Resource):
+    sort_types = ['time', 'bias']
+
     def get(self, event_id):
+        sort = request.args.get('sort')
+        if sort is None:
+            sort = 'time'
+        if sort not in self.sort_types:
+            abort(400, message=f'{sort} is not a valid sort attribute.')
+
+        ascending = request.args.get('ascending')
+        if ascending is None or ascending.lower():
+            ascending = False
+        elif ascending.lower() == 'true':
+            ascending = True
+        else:
+            abort(400, message='ascending needs to be "True" or "False".')
+
+        max_results = request.args.get('max')
+        if max_results is None:
+            max_results = 25
+        elif max_results.isdigit():
+            max_results = int(max_results)
+        else:
+            abort(400, message='max needs to be an integer.')
+
         event = collection.find_one({"eventId": event_id})
-        return event['articles']
+        articles = event['articles']
+
+        if sort == 'time':
+            def key(article): return article['publishedTime']
+        else:
+            def key(article): return article['company']['bias']
+
+        articles = sorted(articles, key=key, reverse=(not ascending))
+        return articles[:max_results]
 
 
 class Events(Resource):
+    sort_types = ['latestTime', 'mostCompanies']
+
     def get(self):
+        sort = request.args.get('sort')
+        if sort is None:
+            sort = 'latestTime'
+        if sort not in self.sort_types:
+            abort(400, message=f'{sort} is not a valid sort attribute.')
+
+        ascending = request.args.get('ascending')
+        if ascending is None or ascending.lower():
+            ascending = False
+        elif ascending.lower() == 'true':
+            ascending = True
+        else:
+            abort(400, message='ascending needs to be "True" or "False".')
+
+        max_results = request.args.get('max')
+        if max_results is None:
+            max_results = 25
+        elif max_results.isdigit():
+            max_results = int(max_results)
+        else:
+            abort(400, message='max needs to be an integer.')
 
         events = []
         for event in collection.find():
@@ -54,7 +109,14 @@ class Events(Resource):
                 'latestTime': latest_article['publishedTime'],
                 'companies': list(companies)
             })
-        return events
+
+        if sort == 'mostCompanies':
+            def key(event): return len(event['companies'])
+        else:
+            def key(event): return event[sort]
+
+        events = sorted(events, key=key, reverse=(not ascending))
+        return events[:max_results]
 
 
 api.add_resource(Articles, '/articles/<event_id>')
