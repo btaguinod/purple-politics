aef8607a5314c7e944dd69e4906701d7f000cff6
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-07-26 19:47:45 -0700

    Add queries and home-events endpoint

--- backend/app/app.py
+++ backend/app/app.py
@@ -21,31 +21,33 @@ db = mongoClient.get_database('purplePolitics')
 collection = db.get_collection('events')
 
 
+def get_param(param_name, default, is_acceptable, to_value):
+    param_string = request.args.get(param_name)
+    if param_string is None:
+        param_string = default
+    elif not is_acceptable(param_string):
+        abort(400, message=f'"{param_string}" is not a valid {param_name} '
+                           f'value')
+    return to_value(param_string)
+
+
 class Articles(Resource):
-    sort_types = ['time', 'bias']
+    DEFAULTS = {
+        'sort': 'time',
+        'ascending': 'false',
+        'max': '10',
+        'page': '1'
+    }
 
     def get(self, event_id):
-        sort = request.args.get('sort')
-        if sort is None:
-            sort = 'time'
-        if sort not in self.sort_types:
-            abort(400, message=f'{sort} is not a valid sort attribute.')
-
-        ascending = request.args.get('ascending')
-        if ascending is None or ascending.lower():
-            ascending = False
-        elif ascending.lower() == 'true':
-            ascending = True
-        else:
-            abort(400, message='ascending needs to be "True" or "False".')
-
-        max_results = request.args.get('max')
-        if max_results is None:
-            max_results = 25
-        elif max_results.isdigit():
-            max_results = int(max_results)
-        else:
-            abort(400, message='max needs to be an integer.')
+        sort = get_param('sort', 'time',
+                         ['time', 'bias'].__contains__,
+                         lambda x: x)
+        ascending = get_param('ascending', 'false',
+                              ['true', 'false'].__contains__,
+                              lambda x: x == 'true')
+        max_results = get_param('max', self.DEFAULTS['max'], str.isdigit, int)
+        page = get_param('page', self.DEFAULTS['page'], str.isdigit, int)
 
         event = collection.find_one({"eventId": event_id})
         articles = event['articles']
@@ -56,35 +58,40 @@ class Articles(Resource):
             def key(article): return article['company']['bias']
 
         articles = sorted(articles, key=key, reverse=(not ascending))
-        return articles[:max_results]
+        return {
+            'count': len(articles),
+            'articles': articles[:max_results * page]
+        }
 
 
 class Events(Resource):
-    sort_types = ['latestTime', 'mostCompanies']
+    DEFAULTS = {
+        'sort': 'latestTime',
+        'ascending': 'false',
+        'max': '10',
+        'page': '1'
+    }
+
+    SORT_TYPES = ['latestTime', 'uniqueCompanies']
 
     def get(self):
-        sort = request.args.get('sort')
-        if sort is None:
-            sort = 'latestTime'
-        if sort not in self.sort_types:
-            abort(400, message=f'{sort} is not a valid sort attribute.')
+        sort = get_param('sort', self.DEFAULTS['sort'],
+                         self.SORT_TYPES.__contains__,
+                         lambda x: x)
+        ascending = get_param('ascending', self.DEFAULTS['ascending'],
+                              ['true', 'false'].__contains__,
+                              lambda x: x == 'true')
+        max_results = get_param('max', self.DEFAULTS['max'], str.isdigit, int)
+        page = get_param('page', self.DEFAULTS['page'], str.isdigit, int)
 
-        ascending = request.args.get('ascending')
-        if ascending is None or ascending.lower():
-            ascending = False
-        elif ascending.lower() == 'true':
-            ascending = True
-        else:
-            abort(400, message='ascending needs to be "True" or "False".')
+        events = self.get_database_events()
+        events = self.sort_events(events, sort, ascending)
+        return {
+            'count': len(events),
+            'events': events[:max_results * page]
+        }
 
-        max_results = request.args.get('max')
-        if max_results is None:
-            max_results = 25
-        elif max_results.isdigit():
-            max_results = int(max_results)
-        else:
-            abort(400, message='max needs to be an integer.')
-
+    def get_database_events(self):
         events = []
         for event in collection.find():
             articles = event['articles']
@@ -109,19 +116,69 @@ class Events(Resource):
                 'latestTime': latest_article['publishedTime'],
                 'companies': list(companies)
             })
+        return events
 
-        if sort == 'mostCompanies':
+    def sort_events(self, events, sort, ascending):
+        if sort == 'uniqueCompanies':
             def key(event): return len(event['companies'])
         else:
             def key(event): return event[sort]
 
-        events = sorted(events, key=key, reverse=(not ascending))
-        return events[:max_results]
+        return sorted(events, key=key, reverse=(not ascending))
+
+class HomeEvents(Events):
+    DEFAULTS = {
+        'cardsSort': 'uniqueCompanies',
+        'cardsAscending': 'false',
+        'cardsMax': '30',
+        'headlinesSort': 'latestTime',
+        'headlinesAscending': 'false',
+        'headlinesMax': '7'
+    }
+    def get(self):
+        cards_sort = get_param('cardsSort', self.DEFAULTS['cardsSort'],
+                               self.SORT_TYPES.__contains__,
+                               lambda x: x)
+        cards_ascending = get_param('cardsAscending',
+                                    self.DEFAULTS['cardsAscending'],
+                                    ['true', 'false'].__contains__,
+                                    lambda x: x == 'true')
+        cards_max_results = get_param('cardsMax', self.DEFAULTS['cardsMax'],
+                                      str.isdigit, int)
+        headlines_sort = get_param('headlinesSort',
+                                   self.DEFAULTS['headlinesSort'],
+                                   self.SORT_TYPES.__contains__,
+                                   lambda x: x)
+        headlines_ascending = get_param('headlinesAscending',
+                                        self.DEFAULTS['headlinesAscending'],
+                                        ['true', 'false'].__contains__,
+                                        lambda x: x == 'true')
+        headlines_max_results = get_param('headlinesMax',
+                                          self.DEFAULTS['headlinesMax'],
+                                          str.isdigit, int)
+
+        events = self.get_database_events()
+
+        card_events = self.sort_events(events, cards_sort, cards_ascending)
+        headline_events = self.sort_events(events, headlines_sort,
+                                           headlines_ascending)
+
+        return {
+            'cardEvents': card_events[:cards_max_results],
+            'headlineEvents': headline_events[:headlines_max_results]
+        }
+
+
+class Base(Resource):
+    def get(self):
+        return 'Welcome! supported endpoints are /articles/<event_id>, ' \
+               '/events, and /home-events'
 
 
 api.add_resource(Articles, '/articles/<event_id>')
 api.add_resource(Events, '/events')
-
+api.add_resource(HomeEvents, '/home-events')
+api.add_resource(Base, '/')
 
 if __name__ == "__main__":
     app.run(debug=True)
