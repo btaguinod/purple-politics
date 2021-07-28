7a45c9a03419c08111aef99b338f2bff83b828ed
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-07-27 21:47:01 -0700

    Rewrite mobile CSS convention

--- frontend/website/src/Header/Header.js
+++ frontend/website/src/Header/Header.js
@@ -10,19 +10,19 @@ export default class Header extends Component {
                     <Link id="logo" to="/">
                         <span id="purple">PURPLE</span> POLITICS
                     </Link>
-                    <div id="button-mobile">
-                        <input type="checkbox" id="button-checkbox-mobile" />
-                        <div id="page-links-mobile">
-                        <Link className="page-link-mobile" to="/"> Home </Link>
-                        <Link className="page-link-mobile" to="/about"> About </Link>
+                    <div id="button">
+                        <input type="checkbox" id="button-checkbox" className="mobile" />
+                        <div className="page-links mobile">
+                            <Link className="page-link" to="/">Home</Link>
+                            <Link className="page-link" to="/about">About</Link>
                         </div>
                         <span />
                         <span />
                         <span />
                     </div>
-                    <div id="page-links">
-                        <Link className="page-link" to="/"> Home </Link>
-                        <Link className="page-link" to="/about"> About </Link>
+                    <div className="page-links">
+                        <Link className="page-link" to="/">Home</Link>
+                        <Link className="page-link" to="/about">About</Link>
                     </div>
                 </div>
             </header>
