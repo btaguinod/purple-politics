885086527cacdf2e0ec9e8c1c052b704d42f0025
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-08-07 17:37:23 -0700

    Add time range to article collector

--- backend/scheduled-function/article_collector.py
+++ backend/scheduled-function/article_collector.py
@@ -1,6 +1,20 @@
 from article import Article
 import feedparser
 import time
+from datetime import datetime, timedelta, timezone
+
+
+def curr_time(offset: int = 0) -> str:
+    """Returns current time with an offset.
+
+    Args:
+        offset (int): Number of offset days.
+
+    Returns:
+        str: time iso format.
+    """
+
+    return str((datetime.now(timezone.utc) + timedelta(offset)).isoformat())
 
 
 class ArticleCollector:
@@ -9,18 +23,23 @@ class ArticleCollector:
     Attributes:
         company (str): Company name.
         url (str): RSS source URL.
-
     """
 
     def __init__(self, company, url):
         self.company = company
         self.url = url
 
-    def get_articles(self, max_articles: int) -> list[Article]:
+    def get_articles(self, max_articles: int, start_time: str = curr_time(-2),
+                     end_time: str = curr_time(0)) -> list[Article]:
         """Get news articles using RSS feed.
 
-        Attributes:
-            max_articles: Maximum number of articles to return
+        Args:
+            max_articles (int): Maximum number of articles to return.
+            start_time (str): Earliest time to return.
+            end_time (str): Latest time to return.
+
+        Returns:
+            list[Article]: Articles.
         """
 
         articles = []
@@ -28,25 +47,27 @@ class ArticleCollector:
         for entry in entries[:max_articles]:
             if 'published_parsed' not in entry:
                 continue
-            title = entry['title']
-            description = self._remove_html_tags(entry['summary'])
-            article_url = entry['link']
-            image_url = self._get_image_url(entry)
             published_time = time.strftime('%Y-%m-%dT%H:%M:%SZ',
                                            entry['published_parsed'])
+            if published_time < start_time or published_time > end_time:
+                continue
+            title = entry['title']
+            description = self.remove_html_tags(entry['summary'])
+            article_url = entry['link']
+            image_url = self.get_image_url(entry)
             article = Article(self.company, title, description, published_time,
                               article_url, image_url)
             articles.append(article)
         return articles
 
     @staticmethod
-    def _remove_html_tags(text: str) -> str:
+    def remove_html_tags(text: str) -> str:
         while '<' in text and '>' in text:
             text = text[:text.find('<')] + text[text.find('>') + 1:]
         return text
 
     @staticmethod
-    def _get_image_url(entry: dict) -> str:
+    def get_image_url(entry: dict) -> str:
         if 'media_content' not in entry:
             return ''
 
