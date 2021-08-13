a5c485fd94a9847c233c07190aa839c644316200
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-08-13 09:24:34 -0700

    Add search query to events page

--- frontend/website/src/NewsEvents/NewsEvents.js
+++ frontend/website/src/NewsEvents/NewsEvents.js
@@ -1,51 +1,67 @@
-import { useEffect, useState, useCallback } from 'react'
-import config from '../config'
-import NewsEvent from './NewsEvent'
-import Sidebar from '../Sidebar/Sidebar'
-import './NewsEvents.css'
+import { useEffect, useState, useCallback } from 'react';
+import { useLocation } from 'react-router';
+import config from '../config';
+import NewsEvent from './NewsEvent';
+import Sidebar from '../Sidebar/Sidebar';
+import './NewsEvents.css';
 
-const sortOptions = ['Date', 'Companies']
-const sortParams = {
+const useQuery = () => {
+    return new URLSearchParams(useLocation().search).get('query');
+};
+
+const defaultSortOptions = ['Date', 'Companies'];
+const defaultSortParams = {
     'Date': 'latestTime',
     'Companies': 'uniqueCompanies'
-}
-const orderOptions = ['Ascending', 'Descending']
+};
+const searchSortOptions = ['Relevance', ...defaultSortOptions];
+const searchSortParams = {'Relevance': 'relevance', ...defaultSortParams};
+const orderOptions = ['Ascending', 'Descending'];
 const orderParams = {
     'Ascending': 'true',
     'Descending': 'false'
-}
-const maxResults = 20
+};
+const maxResults = 20;
 
-export default function NewsEvents() {
-    const [newsEvents, setNewsEvents] = useState([])
-    const [sort, setSort] = useState('Date')
-    const [order, setOrder] = useState('Descending')
+export default function NewsEvents(props) {
+    const query = useQuery();
 
-    const [allLoaded, setAllLoaded] = useState(false)
-    const [nextPageLoaded, setNextPageLoaded] = useState(false)
-    const [page, setPage] = useState(1)
+    const [newsEvents, setNewsEvents] = useState([]);
+    const [sort, setSort] = useState(query ? 'Relevance' : 'Companies');
+    const [order, setOrder] = useState('Descending');
+
+    const [allLoaded, setAllLoaded] = useState(false);
+    const [nextPageLoaded, setNextPageLoaded] = useState(false);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
-        const abortController = new AbortController()
-        const signal = abortController.signal
+        setSort(query ? 'Relevance' : 'Companies')
+    }, [query])
+
+    useEffect(() => {
+        const abortController = new AbortController();
+        const signal = abortController.signal;
+
+        let sortParams = query ? searchSortParams : defaultSortParams;
 
         let sortQuery = 'sort=' + sortParams[sort];
         let orderQuery = 'ascending=' + orderParams[order];
         let maxResultsQuery = 'max=' + maxResults;
         let pageQuery = 'page=' + page;
-        let queries = '?' + sortQuery + '&' + orderQuery + '&' + maxResultsQuery + '&' + pageQuery;
+        let searchQuery = query ? 'query=' + query : '';
+        let queries = `?${sortQuery}&${orderQuery}&${maxResultsQuery}&${pageQuery}&${searchQuery}`;
         fetch(config.backendUrl + '/events' + queries, {signal})
             .then(response => response.json())
             .then(data => {
-                let dataEvents = data['events']
+                let dataEvents = data['events'];
                 setNewsEvents(prevEvents => prevEvents.concat(dataEvents));
-                setNextPageLoaded(true)
+                setNextPageLoaded(true);
                 if (dataEvents.length < maxResults) 
                     setAllLoaded(true);
             })
-            .catch(error => console.log(error))
+            .catch(error => console.log(error));
         return () => abortController.abort()
-    }, [order, sort, page])
+    }, [order, sort, page, query]);
 
     const handleScroll = useCallback(() => {
         let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
@@ -53,30 +69,33 @@ export default function NewsEvents() {
                            document.documentElement.clientHeight - 
                            scrollTop;
         if (distToBottom < 50 && !allLoaded && nextPageLoaded) {
-            setNextPageLoaded(false)
-            setPage(page + 1)
+            setNextPageLoaded(false);
+            setPage(page + 1);
         }
-    }, [nextPageLoaded, allLoaded, page])
+    }, [nextPageLoaded, allLoaded, page]);
 
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
-        return () => window.removeEventListener('scroll', handleScroll)
-    }, [handleScroll])
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [handleScroll]);
 
     const updateSort = newSort => {
-        setPage(1)
-        setAllLoaded(false)
-        setNewsEvents([])
-        setSort(newSort)
-    }
+        setPage(1);
+        setAllLoaded(false);
+        setNewsEvents([]);
+        setSort(newSort);
+    };
 
     const updateOrder = newOrder => {
-        setPage(1)
-        setAllLoaded(false)
-        setNewsEvents([])
-        setOrder(newOrder)
-    }
+        setPage(1);
+        setAllLoaded(false);
+        setNewsEvents([]);
+        setOrder(newOrder);
+    };
+
+    let sortOptions = query ? searchSortOptions : defaultSortOptions;
+    let title = query ? `Search Results for "${query}"` : 'All Events';
 
     return (
         <div id="news-events-container">
@@ -89,7 +108,7 @@ export default function NewsEvents() {
                 setOrder={updateOrder}
             />
             <div id="news-events">
-                <div id="news-events-heading">All Events</div>
+                <div id="news-events-heading">{title}</div>
                 <Sidebar 
                         sort={sort}
                         sortOptions={sortOptions}
@@ -104,5 +123,5 @@ export default function NewsEvents() {
                 )}
             </div>
         </div>
-    )
+    );
 }
